f73b08ca41c59d136d099d126dc1142cc67f3059
Derfrix <derfrix@gmail.com>
2024-07-22 15:55:47 +0000

    Agregar archivos iniciales del módulo Python restart

--- while-loop.py
+++ while-loop.py
@@ -6,14 +6,27 @@ print("Eso es todo!")
 print("...🧙️")
 print("Ya tengo un numero 👀✨")
 
-Mn=random.randint(1, 100)
-Adivino=False
-intento=0
-max_intento=8
+Mn = random.randint(1, 100)
+Adivino = False
+intento = 0
+max_intento = 7
+max_intentoFallas = 3
 
 while not Adivino and intento < max_intento:
-    Yn=int(input("Ahora, bien, dinos tu numero: "))
+    intentoF = 0
+    while intentoF < max_intentoFallas:
+        try:
+            Yn = int(input("Ahora, bien, dinos tu numero: "))
+            break
+        except ValueError:
+            print("Por favor, ingresa un numero valido.")
+        intentoF += 1
+
     intento += 1
+
+    if intentoF == max_intentoFallas:
+        print("Creo que has fallado a proposito 😕")
+        break
 
     if Yn == Mn:
         print(f"Lo has logrado! Tu numero {Yn} es el mismo que pensaba!")
@@ -25,7 +38,11 @@ while not Adivino and intento < max_intento:
     elif Yn < Mn:
         print("Más arriba.")
 
-if not Adivino:
-    print(f"Lo siento 😢")  
+if not Adivino and intentoF < max_intentoFallas:
+    print(f"Lo siento 😢")
     print(f"Has agotado tus {max_intento} intentos.")
+    print(f"El numero era {Mn}.")
+elif not Adivino and intentoF == max_intentoFallas:
+    print(f"Lo siento 😢")
+    print(f"Has agotado tus {max_intentoFallas} intentos de fallas.")
     print(f"El numero era {Mn}.")
